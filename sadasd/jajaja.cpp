926e2a9919393e6246f2d23edf28904c9ff9cb23
NguyenAnhKhoi <1651010083khoi@ou.edu.vn>
2018-10-02 14:44:43 +0700

    Updated

--- sadasd/jajaja.cpp
+++ sadasd/jajaja.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 void main ()
 {
-	cout <<"Hello world" << endl;
+	int a,b;
+	cout <<"Nhap a: " ;
+	cin >> a;
+	cout <<"Nhap b: " ;
+	cin >> b;
+	cout <<"Ket qua: " << (a+b) << endl;
 	system("pause");
 	return;
 }
