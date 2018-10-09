045e7cf3a6ce838b67c3eff2c4272e4a82919187
Minh Tri <1651010151tri@ou.edu.vn>
2018-10-09 15:00:49 +0700

    new update c++

--- sadasd/jajaja.cpp
+++ sadasd/jajaja.cpp
@@ -5,16 +5,8 @@ using namespace std;
 void main ()
 {
 	int a,b;
-<<<<<<< HEAD
-	cout << " nhap a, b: " << endl;
-	cin >> a>>b;
-=======
-	cout <<"Nhap a: " ;
-	cin >> a;
-	cout <<"Nhap b: " ;
-	cin >> b;
-	cout <<"Ket qua: " << (a+b) << endl;
+	cout <<"Nhap ab: " ; cin >> a >> b;
+	cout <<"result: " << (a+b) << endl;
 	system("pause");
->>>>>>> 926e2a9919393e6246f2d23edf28904c9ff9cb23
 	return;
 }
